5fbcb3b8652ac599ad053ab312f132d4a801d61e
Jony Malhotra <jony.malhotra82@gmail.com>
2013-09-13 18:28:25 +0530

    bootstrap responsive layout template - initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrowthValley::Application.config.secret_token = '41d54928902b732e2ea338a80433af760d8588f424cdcbe3624d363ed0c6fbeb441b86523c8e6ae5bfb2a1c433f0aaf462eb557bbdbd358b72168c238c2f4a3f'
