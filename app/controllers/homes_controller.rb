5e1595b33adc7e7400a212c1fea3e2c700125b33
Jony Malhotra <jony.malhotra82@gmail.com>
2013-09-17 18:39:28 +0530

    added dummy homepage

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,6 @@
+class HomesController < ApplicationController
+  skip_before_filter :authenticate_user!
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 GrowthValley::Application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :subjects
+  resources :homes, only: :index
+
+  root to: 'homes#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
