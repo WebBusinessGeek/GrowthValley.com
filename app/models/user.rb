497df97ddff9c3460d710669ace0b9f9d3aa9a7a
Jony Malhotra <jony.malhotra82@gmail.com>
2013-10-07 11:25:00 +0530

    enabled confirmation email module for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :full_name, :description, :type, :age, :sex, :subscription_type, :subject_ids, :course_ids
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 GrowthValley::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['MAILER_USERNAME'],
+  :password => ENV['MAILER_PWD'],
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
