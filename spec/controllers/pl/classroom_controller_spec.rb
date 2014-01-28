b2f35b68cab5555e7c9346544f784224ad6db522
Adam Robbie <adam.robbie@gmail.com>
2014-01-28 18:51:06 -0500

    Classroom controller specs

--- spec/controllers/pl/classroom_controller_spec.rb
+++ spec/controllers/pl/classroom_controller_spec.rb
@@ -3,8 +3,32 @@ require 'spec_helper'
 describe Pl::ClassroomsController, type: :controller do
   context 'teacher' do
     login_as_teacher
+
+    it 'get #index' do
+      get :index
+      response.should be_success
+    end
+
+    it 'get #show' do
+      classroom = FactoryGirl.create(:classroom)
+      # current_user.classrooms << classroom
+      get :show, id: classroom
+      assigns(:classroom).should == classroom
+    end
   end
   context 'learner' do
     login_as_learner
+
+    it 'get #index' do
+      get :index
+      response.should be_success
+    end
+
+    it 'get #show' do
+      classroom = FactoryGirl.create(:classroom)
+      # current_user.classrooms << classroom
+      get :show, id: classroom
+      assigns(:classroom).should == classroom
+    end
   end
 end
