3a0781ef96bf47b61c6ad64a233a6bafec7c7929
Adam Robbie <adam.robbie@gmail.com>
2014-02-04 10:00:33 -0500

    Human readable timestamps on comments and checklist task order

--- app/models/pl/checklist_teachable.rb
+++ app/models/pl/checklist_teachable.rb
@@ -1,7 +1,7 @@
 class Pl::ChecklistTeachable < ActiveRecord::Base
   attr_accessible :tasks_attributes, :content
   has_one :content, as: :teachable
-  has_many :tasks, foreign_key: 'checklist_id', dependent: :destroy
+  has_many :tasks, order: 'created_at ASC', foreign_key: 'checklist_id', dependent: :destroy
 
   accepts_nested_attributes_for :tasks, allow_destroy: true
 end
