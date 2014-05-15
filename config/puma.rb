4cb2db07b2dec75be2f03fa1493927076d4dbc30
Adam Robbie <adam.robbie@gmail.com>
2014-05-15 11:50:37 -0400

    puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,6 @@
 # config/puma.rb
 threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
 
-workers Integer(ENV['PUMA_WORKERS'] || 1)
-
 rackup DefaultRackup
 port ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
@@ -11,10 +9,5 @@ preload_app!
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
     ActiveRecord::Base.establish_connection
-  end
-
-   if defined?(Sidekiq)
-    Sidekiq.redis = ENV["REDISTOGO_URL"] || "redis://127.0.0.1:6379"
-    Rails.logger.info('Connected to Redis')
   end
 end
